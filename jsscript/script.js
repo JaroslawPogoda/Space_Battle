a6f1c89e37cb7ff8b37a5b9c431bf207972f24ec
Jaroslaw Pogoda <jarek1330@gmail.com>
2021-12-20 16:41:08 -0500

    setting up classes

---
+++ jsscript/script.js
@@ -0,0 +1,19 @@
+class ship{
+    hitPoints=0;
+    firePower =0
+    accuracy =0
+}
+class enemy extends ship{
+    constructor(hp,firePower,){
+        this.hitPoints=hp;
+        this.firePower=firePower;
+        this.accuracy=accuracy;
+    }
+}
+class player extends ship{
+    constructor(){
+        this.hitPoints=hp;
+        this.firePower=firePower;
+        this.accuracy=accuracy;
+    }
+}
