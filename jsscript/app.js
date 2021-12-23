6189b17b76552ef52c33817441c3c745278d224e
Jaroslaw Pogoda <jarek1330@gmail.com>
2021-12-23 09:12:27 -0500

    uploaded some animation inprogress

--- jsscript/app.js
+++ jsscript/app.js
@@ -4,8 +4,10 @@
  *            completed 3. create Retreat screen
  *            partialy completed 4. Create choose your difficulty level
  *            completed 5. use accuracy to determine luck of hit
- *             6. balancing the game
+ *            Partially completed 6. balancing the game
+ *            6.Comment code
  */
+//global variable declarations and html tag retreval for further manipulation
 let prompt = "easy"
 let playerStatDisplay=document.querySelector(".playerStats")
 let enemyStatDisplay=document.querySelector(".enemyStats")
@@ -13,122 +15,146 @@ let endGameMessage =document.getElementById("endGameScreen")
 let gameContainer = document.querySelector(".bodyContainer") 
 let newGame=""
 let startMenuElements= document.querySelector("#startMenu")
+let difEnemies=[6,7,8]
+// end of declarations for global variables
+
+//declaration of function Reeveal game elements this function hides all
+// menu elements and lets game to be playable, No parameters
 function revealGameElements(){
     startMenuElements.style.display="none"
     gameContainer.style.display="flex"
-}
+}//end of revealGameElements function
+
+//Class parent dectaration Ship
 class Ship{
+    //properties of ship class
     hitPoints=0;
     firePower =0
     accuracy =0
+    //constructor for class ship Takes hull point fire power and accuracy as parameter
     constructor(hp,firePower,accuracy){
         this.hitPoints=hp;
         this.firePower=firePower;
         this.accuracy=accuracy;
-    }
+    }// end of class ship costructor
+    // method hit this methods subtracts firepower  from hitpoints upon sucessful hit 
     hit(firePower){
         this.hitPoints-=firePower;
         console.log(this.hitPoints)
-    }
-}
+    }//end of the hit method
+}//end of ship class
+
+//Class enemy declaration this method extends ship
 class Enemy extends Ship{
+    //enemy class constructor takes hull point fire power and accuracy as parameter
     constructor(hp,firePower,accuracy){
         super(hp,firePower,accuracy);
-        
-    }
+    }// end of constructor
+    //hit method takes firepower as parameter
     hit(firePower){
         this.hitPoints-=firePower;
         console.log(this.hitPoints)
-    }
+    }//end of hit method
+}//end of enemy Class
 
-}
+//player class entends ship
 class Player extends Ship{
+    //player class constructor takes hull point fire power and accuracy as parameter
     constructor(hp,firePower,accuracy){
         super(hp,firePower,accuracy)
         playerStatDisplay.innerHTML =` Hull : ${hp} <br> FirePower : ${firePower} <br> Accuracy : ${accuracy} <br>`
-    }
+    }//end  of the constructor
+    //hit method takes firepower as parameter
     hit(firePower){
         this.hitPoints-=firePower;
         console.log(this.hitPoints)
-    }
-}
+    }//end of hit function
+}//end of player class
+//function that hides game tag elements and shows end game elements
 function hideGame(message){
     document.getElementById("endGameMessage").innerHTML=message
     gameContainer.style.display="none";
 
-}
-function createGame(difficulty){}
+}//ene of finction hideGame
+
+// class declaration for game
 class Game{
-    static difEnemies=[6,7,8]
+    //win conditions check function
     checkIfWin(){
         if(this.player.hitPoints<0){
             hideGame("Game Over\nYou Lose")
-        }
+        }//end if
         else if(this.enemy.length<=0){
             hideGame("Congratz\n You are the winner")
-        }
+        }//end else if
         else if(this.enemy[this.enemy.length-1].hitPoints<0){
         this.player.hitPoints= this.player.hitPoints+1
         this.enemy.pop()
         console.log("nextEnemy")
-        }
+        }//end else if
         else{console.log("continue battle")}
-    }
+    }//end of checkIfWin function
+    // method that creats player object takes hull points fire power accuracy
     createPlayer(hp,fp,acc){
         this.player = new Player(hp,fp,acc)
-    }
+    }// end of method create player
+
+    // create enemy creats one enemy object and appends it to the enemy array randomly created
     createEnemy(){
-        let randNumHP = Math.floor(Math.random()*10+1)
-        let randNumFP = Math.floor(Math.random()*4)
-        let randNumACC = Math.random()
+        let randNumHP = Math.floor((Math.random()*3)+3)
+        let randNumFP = Math.floor((Math.random()*2)+2)
+        let randNumACC = (Math.random()*0.2)+0.6
         this.enemy.push(new Enemy(randNumHP,randNumFP,randNumACC))
-    }
+    }// end of create enemy method
+
+    // enemy hit turn this method is caled to to attack player creates random chance for enemy to hit
     enemyHit(player,enemyfirePower){
         let enemyLuck = Math.random();
         if (enemyLuck<this.enemy[this.enemy.length-1].accuracy){
             player.hit(enemyfirePower)
-            console.log("enemy hit")
-        }
-        else console.log("enemy miss")
-    }
-    round(enemyFighting){
+            alert("enemy hit")
+        }// end of if statement
+        else alert("enemy miss")
+    }// end of enemy hit function
+
+    // declaration of round method runs one round of the fight
+    round(){
         //this.checkIfWin()
         //console.log(`Player luck is ${playerLuck} php=${this.player.hitPoints} ehp=${enemyFighting.hitPoints}`)
         let playerLuck = Math.random();
         let enemyLuck = Math.random();
         //console.log(`Player luck is ${playerLuck} php=${this.player.hitPoints} ehp=${this.enemy[this.enemy.length-1].hitPoints}`)
         if(playerLuck<this.player.accuracy&&this.enemy.length>0){
-            console.log(  this.enemy)
-            console.log()
-            enemyStatDisplay.innerHTML=` Hull : ${this.enemy[this.enemy.length-1].hitPoints} <br> FirePower : ${this.enemy[this.enemy.length-1].firePower} <br> Accuracy : ${this.enemy[this.enemy.length-1].accuracy} <br>`
+            alert("player hits")
+            enemyStatDisplay.innerText=` Hull : ${this.enemy[this.enemy.length-1].hitPoints} \n FirePower : ${this.enemy[this.enemy.length-1].firePower} \n Accuracy : ${this.enemy[this.enemy.length-1].accuracy} <br>`
             this.enemy[this.enemy.length-1].hit(this.player.firePower)
             this.enemyHit(this.player,this.enemy[this.enemy.length-1].firePower)
-            }
+            }// end of if statement
         else if(this.enemy.length>0){
-            console.log("Player miss hit")
+            alert("Player miss hit")
             this.enemyHit(this.player,this.enemy[this.enemy.length-1].firePower)
             playerStatDisplay.innerHTML =` Hull : ${this.player.hitPoints} <br> FirePower : ${this.player.firePower} <br> Accuracy : ${this.player.accuracy} <br>`
-        }
+        }// end of else if statement
         this.checkIfWin()
-    }
+    }//end of round method 
     constructor(difficulty){
         this.difficulty=difficulty  
         this.enemy=[]    
         if(this.difficulty.toLowerCase()=="easy"){
             this.createPlayer(20,5,0.6);
-            for(let iterator = 0 ; iterator < 6; iterator++){
+            for(let iterator = 0 ; iterator < difEnemies[0]; iterator++){
                 this.createEnemy();
             }
         }
         else if(this.difficulty.toLowerCase()=="medium"){
             this.createPlayer(15,4,0.5);
-            for(let iterator = 0 ; iterator < 6; iterator++){
+            for(let iterator = 0 ; iterator < difEnemies[1]; iterator++){
                 this.createEnemy();
             }
         }
         else if(this.difficulty.toLowerCase()=="hard"){
             this.createPlayer(10,3,0.4);
-            for(let iterator = 0 ; iterator < 6; iterator++){
+            for(let iterator = 0 ; iterator < difEnemies[2]; iterator++){
                 this.createEnemy();
             }
         }
